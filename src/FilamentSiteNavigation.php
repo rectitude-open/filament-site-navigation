d425e710b7fc7bbd113e31ab890c5c53694f2aa8
aspirantzhang <admin@aspirantzhang.com>
2025-06-30 18:11:58 +0800

    feat: implement getModel method to retrieve the configured model for SiteNavigation

--- src/FilamentSiteNavigation.php
+++ src/FilamentSiteNavigation.php
@@ -4,4 +4,16 @@ declare(strict_types=1);
 
 namespace RectitudeOpen\FilamentSiteNavigation;
 
-class FilamentSiteNavigation {}
+use Illuminate\Database\Eloquent\Model;
+use RectitudeOpen\FilamentSiteNavigation\Models\SiteNavigation;
+
+class FilamentSiteNavigation
+{
+    /**
+     * @return class-string<Model>
+     */
+    public function getModel(): string
+    {
+        return config('filament-site-navigation.model', SiteNavigation::class);
+    }
+}
